1f4f6fcf8ff21fe23ac0f687cc7c185da0231021
kenter <kenter@MacBook-Pro-de-kenter.local>
2022-05-23 08:11:53 +0200

    mroning commit

--- src/Components/MultiForm/SubForms/FoodStyle.js
+++ src/Components/MultiForm/SubForms/FoodStyle.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, {useRef} from 'react';
+import './SubForm.css'
 
 const Foodstyle = () => {
+
+    const preventFunct = () => {
+
+    }
+
     return (
         <div>
-            
+            <form
+            className='checkbox-form'
+            onClick={preventFunc}
+            >
+
+                <p>Quelles sont es cusiines préférée ?</p>
+                <span>Choix multiples.</span>
+
+                <label htmlFor='italian'>
+                    Italienne
+                </label>
+                <input type="text" id='italian' value='italian' /> 
+
+                <label htmlFor='italian'>
+                    Italienne
+                </label>
+                <input type="text" id='italian' value='italian' />
+
+                <label htmlFor='italian'>
+                    Italienne
+                </label>
+                <input type="text" id='italian' value='italian' />               
+
+                <label htmlFor='italian'>
+                    Italienne
+                </label>
+                <input type="text" id='italian' value='italian' />
+
+                <label htmlFor='italian'>
+                    Italienne
+                </label>
+                <input type="text" id='italian' value='italian' />
+
+            </form>
         </div>
     );
 }
